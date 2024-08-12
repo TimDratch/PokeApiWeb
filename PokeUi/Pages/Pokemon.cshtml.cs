e80084a31b1699d644617593e553df215dbda0dc
Timothy Dratch <tdratch@printscan.com>
2024-08-12 10:12:01 -0400

    Fixed bug with pokedex generation due to names call being async and not completing before page being served NOTE: Solution needs refinement

--- PokeUi/Pages/Pokemon.cshtml.cs
+++ PokeUi/Pages/Pokemon.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Flurl.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Principal;
 
 namespace PokeUi.Pages
 {
@@ -8,7 +9,7 @@ namespace PokeUi.Pages
     {
         private readonly ILogger<PokemonModel> _logger;
         public string ImageUrl = "https://localhost:7197/Sprite/";
-        public string PokedexUrl = "https://localhost:7197/Pokemon/";
+        private static readonly string PokedexUrl = "https://localhost:7197/Pokemon/";
         public List<string> PokemonNames = new();
         public Dictionary<string, string> PokedexDic = new();
         public PokemonModel(ILogger<PokemonModel> logger)
@@ -16,12 +17,12 @@ namespace PokeUi.Pages
             _logger = logger;
         }
 
-        public async void OnGet(string pokemonName)
+        public void OnGet(string pokemonName)
         {
             ViewData["pokemonName"] = pokemonName;
             ImageUrl += pokemonName;
-            PokemonNames = await PokedexUrl.GetJsonAsync<List<string>>();
-            foreach(var name in PokemonNames)
+            PokemonNames = PokedexUrl.GetJsonAsync<List<string>>().Result;
+            foreach (var name in PokemonNames)
             {
                 PokedexDic.Add(name, "https://localhost:7197/Sprite/" + name);
             }
